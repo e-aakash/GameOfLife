e96268f87e206d102815b1160bd8e6681e6cb67c
E Aakash <09e.aakash@gmail.com>
2020-08-08 12:07:34 +0530

    feat: add simple form in Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  rows:number = 10;
+  columns:number = 10;
+  submitted:boolean = false;
 
-  constructor() { }
+  constructor() {
+    this.rows = 10;
+    this.columns = 10; 
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.rows, " ", this.columns);
   }
 
 }
